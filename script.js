90b5f42517332aeb137f4c7067070212e2e64221
dorogobit2061-lab <dorogobit2061@gmail.com>
2026-09-04 16:13:11 +0300

    start

---
+++ script.js
@@ -0,0 +1,25 @@
+const name = "Артем";
+console.log(name);
+
+const isStudent = true;
+console.log(isStudent);
+
+const myString = "Talk is cheap. Show me the code.";
+console.log(myString);
+
+let myNumber = 42;
+myNumber += 10;
+console.log(myNumber);
+
+const myNull = null;
+console.log(myNull);
+
+const userName = prompt("Введіть своє ім’я");
+alert(userName);
+console.log(typeof userName);
+
+const userAnswer = confirm("Ви дійсно хочете покинути сторінку?");
+console.log(userAnswer);
+
+alert("Увага! Дія може бути небезпечною.");
+const isConfirmed = confirm("Ви впевнені, що хочете продовжити?");
